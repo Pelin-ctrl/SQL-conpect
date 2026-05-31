65e7bb8badde1897f0a04dbb4dd184cff9d69689
Pelin <pelinakdulun2020@gmail.com>
2026-06-01 00:31:59 +0300

    update

--- sql_firstlesson.sql
+++ sql_firstlesson.sql
@@ -154,6 +154,23 @@ FROM book
 ORDER BY income_percent DESC;
 
 
+CREATE TABLE supply(
+    supply_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50),
+    author VARCHAR(30),
+    price DECIMAL(8,2),
+    amount INT
+);
+
+
+INSERT INTO supply (title, author, price, amount)
+VALUES
+        ('Лирика', 'Пастернак Б.Л.', 518.99, 2),
+        ('Черный человек', 'Есенин С.А.', 570.20, 6),
+        ('Белая гвардия', 'Булгаков М.А.', 540.50, 7),
+        ('Идиот', 'Достоевский Ф.М.', 360.80, 3);
+
+
 
 
 
